a503113eb5f8a8a797b2389f77aa08c97948146e
nguyentanvy <1951120161@sv.ut.edu.vn>
2023-06-07 17:24:38 +0700

    built profile #1

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getDataAPI } from '../../utils/fetchData';
 import { GLOBALTYPES } from '../../redux/actions/globalTypes';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import UserCard from '../UserCard';
+import LoadIcon from "../../images/loading.gif"
 
 const Search = () => {
   const [search, setSearch] = useState('')
@@ -11,25 +12,41 @@ const Search = () => {
 
   const { auth } = useSelector(state => state)
   const dispatch = useDispatch()
-    useEffect(() => {
-      if(search ) {
-        getDataAPI(`search?username=${search}`, auth.token)
-        .then(res => setUsers(res.data.users))
-        .catch(err => {
-          dispatch({
-            type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
-          })
+  const [load, setLoad] = useState(false);
+    // useEffect(() => {
+    //   if(search ) {
+    //     getDataAPI(`search?username=${search}`, auth.token)
+    //     .then(res => setUsers(res.data.users))
+    //     .catch(err => {
+    //       dispatch({
+    //         type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
+    //       })
+    //     })
+    //   }else{
+    //     setUsers([])
+    //   }
+    // }, [search, auth.token, dispatch]);
+    const handleSearch = async (e) => {
+      e.preventDefault();
+      if(!search) return;
+      try{
+        setLoad(true)
+        const res = await getDataAPI(`search?username=${search}`, auth.token)
+        setUsers(res.data.users)
+        setLoad(false)
+      } catch(err){
+        dispatch({
+          type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}
+          
         })
-      }else{
-        setUsers([])
       }
-    }, [search, auth.token, dispatch]);
+    }
     const handleClose = () => {
       setSearch('')
       setUsers([])
     }
   return (
-    <form className="search_form">
+    <form className="search_form" onSubmit={handleSearch}>
         <input type="text" name="search" value={search} id="search"
         onChange={e => setSearch(e.target.value.toLowerCase().replace(/ /g, ''))}
         />
@@ -38,12 +55,19 @@ const Search = () => {
             <span>Search</span>
         </div>
         <div className="close_search" onClick={handleClose} style={{opacity: users.length === 0 ? 0 : 1}}>&times;</div>
+        
+        <button type="submit" style={{display: 'none'}}>Search</button>
+        
+        {load&& <img src={LoadIcon} alt="loading" className="loading"/>}
+        
         <div className="users" >
           {
             search && users.map(user => (
-              <Link key={user._id} to={`/profile/${user._id}`} onClick={handleClose}>
-                <UserCard user={user} border="border"/>
-              </Link>
+            
+                <UserCard key={user._id}  user={user} border="border"
+                handleClose={handleClose}
+                />
+           
             ))
           }
         </div>
