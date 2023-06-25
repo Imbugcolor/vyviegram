7acf0051c1e77155ffd0c8cc4efa94004ebd1f68
nguyentanvy <1951120161@sv.ut.edu.vn>
2023-06-26 02:57:39 +0700

    Update & Like Comment [FE]

--- client/src/components/home/comments/CommentCard.js
+++ client/src/components/home/comments/CommentCard.js
@@ -3,22 +3,49 @@ import Avatar from '../../Avatar'
 import LikeButton from "../../LikeButton"
 import {Link} from 'react-router-dom'
 import moment from 'moment'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import CommentMenu from './CommentMenu'
+import { likeComment, unLikeComment, updateComment } from '../../../redux/actions/commentAction'
 const CommentCard = ({comment, post}) => {
+  const dispatch = useDispatch()
   const {auth} = useSelector(state => state)
   const [content, setContent] = useState('')
   const [readMore, setReadMore] = useState(false)
   const [isLike, setIsLike] = useState(false);
+  const [onEdit, setOnEdit] = useState(false);
+  const [loadLike, setLoadLike] = useState(false)
   useEffect(() => {
       setContent(comment.content)
-  }, [comment]);
+      setIsLike(false)
+      if(comment.likes.find(like => like._id === auth.user._id)){
+        setIsLike(true)
+      }
+  }, [comment, auth.user._id]);
 
-  const handleLike = () => {
-
+  const handleUpdate = () => {
+    if(comment.content !== content)
+    {
+      dispatch(updateComment({comment, post, content, auth}))
+      setOnEdit(false)
+    }
+    else{
+      setOnEdit(false)
+    }
   }
-  const handleUnLike = () => {
 
+  const handleLike = async () => {
+    if(loadLike) return;
+    setIsLike(true)
+    await dispatch(likeComment({comment, post, auth}))
+    setLoadLike(false)
+  }
+  const handleUnLike = async () => {
+    if(loadLike) return;
+    setIsLike(false)
+
+    setLoadLike(true)
+    await dispatch(unLikeComment({comment, post, auth}))
+    setLoadLike(false)
   }
   const styleCard = {
     opacity: comment._id ? 1 : 0.5,
@@ -32,7 +59,13 @@ const CommentCard = ({comment, post}) => {
         </Link>
         <div className="comment_content">
           <div className="flex-fill">
-            <div>
+            {
+              onEdit 
+              ? <textarea rows="5" value = {content} 
+              onChange={e => setContent(e.target.value)}
+              />
+              :
+              <div>
               <span>
                 {
                   content.length < 100 ? content :
@@ -48,25 +81,44 @@ const CommentCard = ({comment, post}) => {
                 </span>
               }
             </div>
+            }
+            
             <div style={{cursor:'pointer'}}>
-                <small className='text-muted mr-3'>
-                    {
-                      moment(comment.createAt).fromNow()
-                    }
+                <small className="text-muted mr-3">
+                    {moment(comment.createdAt).fromNow()}
                 </small>
                 <small className='font-weight-bold mr-3'>
                     {
                       comment.likes.length
                     } likes
                 </small>
-                <small className='font-weight-bold mr-3'>
-                    reply
-                </small>
+
+                {
+                  onEdit 
+                  ? <>
+                      <small className='font-weight-bold mr-3'
+                      onClick={handleUpdate}
+                      >
+                        update
+                     </small>
+
+                     <small className='font-weight-bold mr-3'
+                     onClick={() => setOnEdit(false)}
+                     >
+                        cancel
+                     </small>
+                    </>
+                  :  <small className='font-weight-bold mr-3'>
+                        reply
+                     </small>
+                }
+
+               
             </div>
           
           </div>
           <div className='d-flex align-items-center mr-2' style={{cursor: 'pointer'}}>
-            <CommentMenu post={post} comment={comment} auth = {auth}/>
+            <CommentMenu post={post} comment={comment} auth = {auth} setOnEdit={setOnEdit}/>
             <LikeButton isLike={isLike} handleLike={handleLike} handleUnLike={handleUnLike}/>
           </div>
         </div>
