5968ad96fc670f74d316edb057c1b9f286f5d9a7
nguyentanvy <1951120161@sv.ut.edu.vn>
2023-08-10 17:22:48 +0700

    css login form [FE]

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useGoogleLogin } from '@react-oauth/google'
 import { FcGoogle } from 'react-icons/fc'
 import { AiFillGithub } from 'react-icons/ai'
+import TextBrand from '../images/text-logo.png'
 
 const Login = () => {
     const initialState = { email: '', password: '' }
@@ -52,52 +53,131 @@ const Login = () => {
     }
 
     return (
-        <div className="auth_page">
-            <form onSubmit={handleSubmit}>
-                <h3 className="text-uppercase text-center mb-4">VyViegram</h3>
-                <div className="form-group">
-                    <label htmlFor="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder="Enter email" onChange={handleChangeInput} value={email} name="email"/>
-                    <small id="emailHelp" className="form-text text-muted">
-                        We'll never share your email with anyone else.</small>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="exampleInputPassword1">Password</label>
-                    <div className="pass">
+        // <div className="auth_page">
+        //     <form onSubmit={handleSubmit}>
+        //         <h3 className="text-uppercase text-center mb-4">VyViegram</h3>
+        //         <div className="form-group">
+        //             <label htmlFor="exampleInputEmail1">Email address</label>
+        //             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+        //             placeholder="Enter email" onChange={handleChangeInput} value={email} name="email"/>
+        //             <small id="emailHelp" className="form-text text-muted">
+        //                 We'll never share your email with anyone else.</small>
+        //         </div>
+        //         <div className="form-group">
+        //             <label htmlFor="exampleInputPassword1">Password</label>
+        //             <div className="pass">
 
-                        <input type={typePass ? "text" : "password"} className="form-control" id="exampleInputPassword1" placeholder="Password"
-                        onChange={handleChangeInput} value={password} name="password"
-                        />
+        //                 <input type={typePass ? "text" : "password"} className="form-control" id="exampleInputPassword1" placeholder="Password"
+        //                 onChange={handleChangeInput} value={password} name="password"
+        //                 />
                         
-                        <small onClick={() => setTypePass(!typePass)}>
-                            {typePass ? 'Hide' : 'Show'}
+        //                 <small onClick={() => setTypePass(!typePass)}>
+        //                     {typePass ? 'Hide' : 'Show'}
                         
-                        </small>
-                    </div>
+        //                 </small>
+        //             </div>
                     
-                </div>
+        //         </div>
             
-                <button type="submit" className="btn btn-dark w-100"
-                disabled={email && password ? false : true}
-                >Login</button>
-                <p className="my-2">
-                    You don't have an account? <Link to="/register" style={{color:"crimson"}}>Register Now</Link>
-                </p>
+        //         <button type="submit" className="btn btn-dark w-100"
+        //         disabled={email && password ? false : true}
+        //         >Login</button>
+        //         <p className="my-2">
+        //             You don't have an account? <Link to="/register" style={{color:"crimson"}}>Register Now</Link>
+        //         </p>
 
-                <div className='auth_btn google_auth_btn my-2'>
-                    <div onClick={() => handleGoogleLogin()} className='google-btn-login'>        
-                        <FcGoogle /> 
-                        <span className='ml-2'> Continue with Google</span>
-                    </div>
+        //         <div className='auth_btn google_auth_btn my-2'>
+        //             <div onClick={() => handleGoogleLogin()} className='google-btn-login'>        
+        //                 <FcGoogle /> 
+        //                 <span className='ml-2'> Continue with Google</span>
+        //             </div>
+        //         </div>
+        //         <div className='auth_btn github_auth_btn my-2'>
+        //             <div onClick={() => handleGithubLogin()} >
+        //                 <AiFillGithub /> 
+        //                 <span className='ml-2'> Continue with Github</span>
+        //             </div>
+        //         </div>
+        //     </form>
+        // </div>
+
+        <div >
+            <div className="limiter">
+                <div className="container-login100" style={{backgroundImage: 'url("images/bg-01.jpg")'}}>
+                <div className="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+                    <form className="login100-form validate-form" onSubmit={handleSubmit}>
+                        {/* <span className="login100-form-title p-b-49">
+                        VyViegram
+                        </span> */}
+                        <div className="p-b-49">
+                            <img src={TextBrand}  alt="vyviegram_logo" style={{width: '50%', height: '50px', 'object-fit': 'contain', display: 'block', margin: 'auto'}}/>
+                        </div>
+
+                        <div className="wrap-input100 validate-input m-b-23" data-validate="Username is reauired">
+                            <span className="label-input100">Email address</span>
+                            <input className="input100" type="email" name="email" aria-describedby="emailHelp"
+                        placeholder="Enter email" onChange={handleChangeInput} value={email}/>
+                            <span className="focus-input100" data-symbol="" />
+                            
+                        </div>
+                        <div className="wrap-input100 validate-input" data-validate="Password is required">
+                            <span className="label-input100">Password</span>
+                            <input className="input100"  type={typePass ? "text" : "password"} placeholder="Password"
+                            onChange={handleChangeInput} value={password} name="password" />
+                        
+                            <span className="focus-input100" data-symbol="" />
+                            <small onClick={() => setTypePass(!typePass)}>
+                            {typePass ? 'Hide' : 'Show'}
+                            
+                        </small>
+                        </div>
+                        <div className="text-right p-t-8 p-b-31">
+                            <p>
+                                You don't have an account? <Link to="/register" style={{color:"crimson"}}>Register Now</Link>
+                            </p>
+                        </div>
+                        <div className="container-login100-form-btn">
+                            <div className="wrap-login100-form-btn">
+                            <div className="login100-form-bgbtn" />
+                            <button  type="submit" className="login100-form-btn"  disabled={email && password ? false : true}>
+                                Login
+                            </button>
+                            </div>
+                        </div>
+                        <div className="txt1 text-center p-t-54 p-b-20">
+                            <span>
+                            Or Sign Up Using
+                            </span>
+                        </div>
+                        <div className="flex-c-m">
+                            <a href="#" className="login100-social-item bg1">
+                                <div className='auth_btn github_auth_btn my-2'>
+                                    <div onClick={() => handleGithubLogin()} >
+                                        <AiFillGithub /> 
+                                    </div>
+                                </div>
+                            </a>
+                            <a href="#" className="login100-social-item bg2">
+                                <div className='google_auth_btn'>
+                                    <div onClick={() => handleGoogleLogin()} className='google-btn-login'>        
+                                        <FcGoogle /> 
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        {/* <div className="flex-col-c p-t-155">
+                            <span className="txt1 p-b-17">
+                            Or Sign Up Using
+                            </span>
+                            <a href="#" className="txt2">
+                            Sign Up
+                            </a>
+                        </div> */}
+                    </form>
                 </div>
-                <div className='auth_btn github_auth_btn my-2'>
-                    <div onClick={() => handleGithubLogin()} >
-                        <AiFillGithub /> 
-                        <span className='ml-2'> Continue with Github</span>
-                    </div>
                 </div>
-            </form>
+            </div>
+            <div id="dropDownSelect1" />
         </div>
     )
 }
