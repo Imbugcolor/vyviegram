ebb6620e652bca5123ef4ff29c793cedfc021aa1
Imbugcolor <viethd123456@gmail.com>
2023-09-19 17:07:51 +0700

    QR code download

--- client/src/pages/qr.js
+++ client/src/pages/qr.js
@@ -1,35 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import QRCode from "react-qr-code";
 import { BASE_URL } from '../utils/config';
 import { useSelector } from 'react-redux';
+import html2canvas from 'html2canvas'
+
 const Qr = () => {
     const {auth} = useSelector(state=>state)
     const value= `${BASE_URL}/profile/${auth.user._id}`;
-    console.log(auth)
-    const downloadQRCode = () => {
-        // Generate download with use canvas and stream
-        const canvas = document.getElementById("qr-gen");
-        console.log(canvas)
-        const pngUrl = canvas
-          .toDataURL("image/png")
-          .replace("image/png", "image/octet-stream");
-        let downloadLink = document.createElement("a");
-        downloadLink.href = pngUrl;
-        downloadLink.download = `${value}.png`;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      };
+
+    const exportRef = useRef();
+
+    const exportAsImage = async(el, imageFileName) => {
+      const canvas = await html2canvas(el);
+      const image = canvas.toDataURL("image/png", 1.0);
+      downloadImage(image, imageFileName);
+    }
+
+    const downloadImage = (blob, fileName) => {
+      let downloadLink = document.createElement("a");
+      downloadLink.href = blob;
+      downloadLink.download = fileName;
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    };
     
   return (
-    <div className="qrCode" style={{background: 'url("/images/bg-01.jpg")'}}>
-       <div className="box-qrCode" style={{ padding: '16px',  background: 'white' }}>
+    <div className="qrCode" style={{background: 'url("/images/bg-01.jpg")', backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
+       <div className="box-qrCode" 
+       ref={exportRef}
+       style={{ padding: '16px',  background: 'white' }}>
             <QRCode value={ value}
             id="qr-gen"
             level={"H"}
             />
             <h5>{auth.user.username}</h5>
         </div>
+
         <div className="box-qrText"  style={{color: 'white'}}>
             <div >
                 
@@ -41,7 +48,7 @@ const Qr = () => {
             </div>
             <p>
                 Click for{" "}
-                <button className='qr-button' type="button" onClick={downloadQRCode}>
+                <button className='qr-button' type="button" onClick={() => exportAsImage(exportRef.current, `${auth.user.username}-qrcode.png`)}>
                 Download QR Code
                 </button>
             </p>
