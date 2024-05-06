666355d51c34ded593050b1c6e15c43600cc57ac
Imbugcolor <viethd123456@gmail.com>
2024-05-06 16:41:33 +0700

    config cors (2)

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.json())
 
 const corsOptions = { 
     origin: process.env.CLIENT_URL,
-    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",   
+    allowedHeaders: "Access-Control-Allow-Headers,Access-Control-Allow-Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Origin,Cache-Control,Content-Type,X-Token,X-Refresh-Token",   
+    credentials: true,   
+    preflightContinue: false,  
+    optionsSuccessStatus: 204 
 }
 app.use(cors(corsOptions))
 app.use(cookieParser())
