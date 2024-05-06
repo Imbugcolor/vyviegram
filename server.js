ed3e1afb676d3d80767707fae700871f6bf2c08f
Imbugcolor <viethd123456@gmail.com>
2024-05-06 16:34:24 +0700

    config Cors

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
-app.use(cors({
+
+const corsOptions = { 
     origin: process.env.CLIENT_URL,
     credentials: true,
-}))
+}
+app.use(cors(corsOptions))
 app.use(cookieParser())
 
 
