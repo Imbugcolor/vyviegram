8165908dfbbaf2a4a5cba5129b1b0dd9debfbde7
Imbugcolor <viethd123456@gmail.com>
2024-05-06 17:01:02 +0700

    CORS config 5

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ io.on('connection', socket => {
 ExpressPeerServer(http, { path: '/' })
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
     res.header("Access-Control-Allow-Credentials", "true")
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
